0379ae917a010016d0b6c96ac96a168be8a6692a
ParmS-Musale <musaleparm9541@gmail.com>
2025-09-14 11:11:29 +0530

    Majority Element Added..!!

---
+++ MajorityElement/majorityElement.java
@@ -0,0 +1,36 @@
+// Input: arr[] = [1, 1, 2, 1, 3, 5, 1]
+// Output: 1
+// Explanation: Element 1 appears 4 times. Since ⌊7/2⌋ = 3, and 4 > 3, it is the majority element.
+
+// Input: arr[] = [7]
+// Output: 7
+// Explanation: Element 7 appears once. Since ⌊1/2⌋ = 0, and 1 > 0, it is the majority element.
+
+// Input: arr[] = [2, 13]
+// Output: -1
+// Explanation: No element appears more than ⌊2/2⌋ = 1 time, so there is no majority element.
+
+ public class majorityElement{
+
+     public static int majority(int[] arr){
+        int n = arr.length ;
+
+        for (int i=0 ; i<n ; i++){
+            int count = 0 ;
+            for (int j=0 ; j<n ; j++){
+                if (arr[i] == arr[j]){
+                    count++ ;
+                }
+            }
+            if (count > n/2) {
+                return arr[i];
+            }
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1, 1, 2, 1, 3, 5, 1 };
+        System.out.println(majority(arr));
+    
+    }
+}
